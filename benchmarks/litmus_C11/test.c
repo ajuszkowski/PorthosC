cec9d348f3ea39675d92fe693d066d126bbb222d
ajuszkowski <ajuszkowski@ya.ru>
2018-05-30 05:35:29 +0300

    Add test.c for the example of arbitrary cf-graph.

--- benchmarks/litmus_C11/test.c
+++ benchmarks/litmus_C11/test.c
@@ -1,21 +1,39 @@
-//{
-//    int x = 1, *y;
-//}
+{
+    int x = 1, *y;
+}
 
-void test(int &x, int &y) {
+void thread_0(int &x, int &y) {
+    L0: x = 0;
     int r;
-    while ((5 + 4 / 2 * x) % 3 == 1) {
+    while (x * (5 + 4 / 2) % 3 == 1) {
+        if (x != 0) {
+            goto L0; //backward jump
+        }
         if (y > 6)
             continue;
         else if (++y > 7) {
             r = r + 10;
             break;
         }
+        else {
+            goto L1; //forward jump
+        }
         r = 11;
     }
     y = x + 1;
+    L1: x = r;
 }
 
+
+void thread_1() {
+    while (true) {
+        if (x > 7)
+            break;
+    }
+    y = x;
+}
+
+exists (y == x + 1 && thread_0:r > 21)
 
 
 //void P0(int &z) {
